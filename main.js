17133ffa15e805cbc326dd69f53c012d2d3a4513
Miqayel12 <mikayel.p3@tumo.org>
2023-07-19 17:55:35 +0400

    My first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const footballers = [
+    {
+        "footballer": "/jpgs/Alba.jpg",
+        "name": "Ալբա"
+    },
+    {
+        "footballer": "/jpgs/Alli.jpg",
+        "name": "Ալլի"
+    },
+    {
+        "footballer": "/jpgs/Alva.jpg",
+        "name": "Ալվա"
+    },
+    {
+        "footballer": "/jpgs/Araujo.jpg",
+        "name": "Առա"
+    }
+  ]
