23e31409e088e7ef36dc5ce1725975a015de6afb
Miqayel12 <mikayel.p3@tumo.org>
2023-07-26 16:43:44 +0400

    Verjnakan ardyunq

--- main.js
+++ main.js
@@ -78,7 +78,7 @@ const footballers = [
         "footballer": "./jpgs/Haaland.jpg",
         "name": "Էռլինգ Հաալանդ"
     },{
-        "footballer": "./jpgs/Hazard/jpg",
+        "footballer": "./jpgs/Hazard.jpg",
         "name": "Էդեն Հազարդ"
     },{
         "footballer": "./jpgs/Heno.jpg",
@@ -93,10 +93,10 @@ const footballers = [
         "footballer": "./jpgs/Joao.jpg",
         "name": "Ժոաու Ֆելիքս"
     },{
-        "footballer": "./jpgs/Kai/jpg",
+        "footballer": "./jpgs/Kai.jpg",
         "name": "Կաի Հավերտզ"
     },{
-        "footballer": "./jpgs/Kaka/jpg",
+        "footballer": "./jpgs/Kaka.jpg",
         "name": "Կակա"
     },{
         "footballer": "./jpgs/Kane.jpg",
@@ -143,5 +143,119 @@ const footballers = [
     },{
         "footballer": "./jpgs/Marcelo.jpg",
         "name": "Մառսելո"
+    },{
+        "footballer": "./jpgs/Marquinhos.jpg",
+        "name": "Մառքուինյոս"
+    },{
+        "footballer": "./jpgs/Mbappe.jpg",
+        "name": "Մբապպե"
+    },{
+        "footballer": "./jpgs/Messi.jpg",
+        "name": "Մեսսի"
+    },{
+        "footballer": "./jpgs/Modric.jpg",
+        "name": "Մոդրիչ"
+    },{
+        "footballer": "./jpgs/Mount.jpg",
+        "name": "Մեյսոն Մաունտ"
+    },{
+        "footballer": "./jpgs/Muller.jpg",
+        "name": "Թոմաս Մյուլլեռ"
+    },{
+        "footballer": "./jpgs/Neuer.jpg",
+        "name": "Մանուել Նոյեռ"
+    },{
+        "footballer": "./jpgs/Neymar.jpg",
+        "name": "Նեյմառ"
+    },{
+        "footballer": "./jpgs/Nunez.jpg",
+        "name": "Դառվին Նունյեզ"
+    },{
+        "footballer": "./jpgs/Onana.jpg",
+        "name": "Օնանա"
+    },{
+        "footballer": "./jpgs/Owen.jpg",
+        "name": "Միքայել Օվեն"
+    },{
+        "footballer": "./jpgs/Ozil.jpg",
+        "name": "ՄԵսութ Օզիլ"
+    },{
+        "footballer": "./jpgs/Pedri.jpg",
+        "name": "Պեդռի"
+    },{
+        "footballer": "./jpgs/Perisic.jpg",
+        "name": "Իվան Պերիշիչ"
+    },{
+        "footballer": "./jpgs/Pique.jpg",
+        "name": "Պիքե"
+    },{
+        "footballer": "./jpgs/Pogba.jpg",
+        "name": "Պաուլ Պօգբա"
+    },{
+        "footballer": "./jpgs/Pulisic.jpg",
+        "name": "Քրիստիան Պուլիշիչ"
+    },{
+        "footballer": "./jpgs/Rakitic.jpg",
+        "name": "Իվան Ռակիտիչ"
+    },{
+        "footballer": "./jpgs/Ramos.jpg",
+        "name": "Սեռգիո Ռամոս"
+    },{
+        "footballer": "./jpgs/Raphi.jpg",
+        "name": "Ռաֆինյա"
+    },{
+        "footballer": "./jpgs/Roberto.jpg",
+        "name": "Ռոբեռտո Ֆիռմինյո"
+    },{
+        "footballer": "./jpgs/Rodrygo.jpg",
+        "name": "Ռոդրիգո"
+    },{
+        "footballer": "./jpgs/Ronaldo.jpg",
+        "name": "Քրիստիանո Ռոնալդո"
+    },{
+        "footballer": "./jpgs/Salah.jpg",
+        "name": "Մոհամմեդ Սալահ"
+    },{
+        "footballer": "./jpgs/Sanches.jpg",
+        "name": "Սանչեզ"
+    },{
+        "footballer": "./jpgs/Sane.jpg",
+        "name": "Լեռոյ Սանե"
+    },{
+        "footballer": "./jpgs/Sergio.jpg",
+        "name": "Սեռխիո Ռոբեռտո"
+    },{
+        "footballer": "./jpgs/Silva.jpg",
+        "name": "Բեռնառդու Սիլվա"
+    },{
+        "footballer": "./jpgs/Son.jpg",
+        "name": "Սոն Խյոնգ Մին"
+    },{
+        "footballer": "./jpgs/Stegen.jpg",
+        "name": "Տեռ Շտեգեն"
+    },{
+        "footballer": "./jpgs/Suarez.jpg",
+        "name": "Լուիզ Սուառեզ"
+    },{
+        "footballer": "./jpgs/Theo.jpg",
+        "name": "Տէո Հեռնանդեզ"
+    },{
+        "footballer": "./jpgs/Upamecano.jpg",
+        "name": "Ուպամեկանո"
+    },{
+        "footballer": "./jpgs/Valdes.jpg",
+        "name": "Վիկտոռ Վալդես"
+    },{
+        "footballer": "./jpgs/Vidal.jpg",
+        "name": "Արթութ Վիդալ"
+    },{
+        "footballer": "./jpgs/Vini.jpg",
+        "name": "Վինիսիուս Ժունիոր"
+    },{
+        "footballer": "./jpgs/Xavi.jpg",
+        "name": "Խավի"
+    },{
+        "footballer": "./jpgs/zanetti.jpg",
+        "name": "Զանետտի"
     }
   ]
